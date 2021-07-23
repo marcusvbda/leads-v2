40f0ecd331824e4a5b0dfc5f744ff406645e5d4e
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2021-07-23 01:26:34 -0300

    painel

---
+++ resources/js/components/Leads/LeadAttendance.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="attendance">
+        <div class="row">
+            <category-side />
+            <lead-painel />
+        </div>
+    </div>
+</template>
+<script>
+import leadStore from '~/stores/lead'
+
+export default {
+    props: ['user'],
+    store: leadStore,
+    components: {
+        'category-side': require('./partials/-category-side.vue').default,
+        'lead-painel': require('./partials/painel/-lead-painel.vue').default,
+    },
+    created() {
+        this.init()
+    },
+    methods: {
+        init() {
+            this.$store.commit('setUser', this.user)
+        },
+    },
+}
+</script>
---
+++ resources/js/components/Leads/partials/-lead-card.vue
@@ -0,0 +1,57 @@
+<template>
+    <div :class="`d-flex flex-row py-3 px-1 lead-card f-12 ${selected_lead.id == lead.id ? 'selected' : ''}`" @click="select">
+        <h3 class="el-icon-circle-plus" />
+        <div class="d-flex flex-column ml-2 flex-grow-1">
+            <div class="d-flex flex-row justify-content-between">
+                <b>{{ lead.name }}</b>
+                <v-runtime-template :template="lead.f_rating" />
+            </div>
+            <div class="d-flex flex-row justify-content-between">
+                <span class="text-muted">{{ lead.email }}</span>
+                <span class="text-muted">{{ lead.status.name }}</span>
+            </div>
+            <div class="d-flex flex-row justify-content-between">
+                <resource-tags-input resource="leads" :resource_code="lead.code" only_view style="zoom: 0.7" class="mt-2" />
+                <span class="text-muted">{{ lead.f_created_at }}</span>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import VRuntimeTemplate from 'v-runtime-template'
+export default {
+    props: ['lead'],
+    components: {
+        VRuntimeTemplate,
+    },
+    computed: {
+        selected_lead() {
+            return this.$store.state.lead
+        },
+    },
+    methods: {
+        select() {
+            if (this.selected_lead.id == this.lead.id) {
+                return this.$store.commit('setLead', {})
+            }
+            this.$store.commit('setLead', this.lead)
+        },
+    },
+}
+</script>
+<style lang="scss">
+.lead-card {
+    cursor: pointer;
+    border-bottom: 1px solid #eeeeee;
+    &:hover {
+        background-color: #f5f5f5;
+        transition: 0.3s;
+    }
+    &.selected {
+        background-color: #dff3ff;
+        &:hover {
+            background-color: #0074d921;
+        }
+    }
+}
+</style>
