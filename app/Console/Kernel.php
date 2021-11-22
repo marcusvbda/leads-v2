ea34efe96c24e26ac09fd558acac41a1f457a4b2
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2021-11-21 23:35:35 -0300

    schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,15 +13,33 @@ class Kernel extends ConsoleKernel
 
 	protected function schedule(Schedule $schedule)
 	{
-		$schedule
-			->command('queue:work --queue=resource-import,resource-export,alert-broadcasts,event-broadcasts')
-			->everyMinute()
-			->withoutOverlapping();
+		if (!$this->osProcessIsRunning('queue:work')) {
+			$schedule
+				->command('queue:work --queue=resource-import,resource-export,alert-broadcasts,event-broadcasts')
+				->everyMinute();
+		}
 	}
 
 	protected function commands()
 	{
 		// $this->load(__DIR__ . '/Commands');
 		require base_path('routes/console.php');
+	}
+
+	protected function osProcessIsRunning($needle)
+	{
+		// get process status. the "-ww"-option is important to get the full output!
+		exec('ps aux -ww', $process_status);
+
+		// search $needle in process status
+		$result = array_filter($process_status, function ($var) use ($needle) {
+			return strpos($var, $needle);
+		});
+
+		// if the result is not empty, the needle exists in running processes
+		if (!empty($result)) {
+			return true;
+		}
+		return false;
 	}
 }
