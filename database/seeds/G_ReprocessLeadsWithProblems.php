d1d4771294899fa3de333a432a8046c4ef4059a9
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2021-11-26 13:54:18 -0300

    comenta

--- database/seeds/G_ReprocessLeadsWithProblems.php
+++ database/seeds/G_ReprocessLeadsWithProblems.php
@@ -12,19 +12,20 @@ class G_ReprocessLeadsWithProblems extends Seeder
 {
 	public function run()
 	{
-		DB::statement('SET AUTOCOMMIT=0;');
-		DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-		$controller = new WebhookController();
-		$webhook = Webhook::first();
-		foreach ($this->getLeadsWithProblems() as $lead) {
-			$content = @$lead->webhook_request->content;
-			if ($content) {
-				$controller->handler($webhook->token, new Request((array) ["lead_api" => $content]), $lead->id);
-			}
-		}
-		DB::statement('SET FOREIGN_KEY_CHECKS=1;');
-		DB::statement('SET AUTOCOMMIT=1;');
-		DB::statement('COMMIT;');
+		// DB::statement('SET AUTOCOMMIT=0;');
+		// DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+		// $controller = new WebhookController();
+		// $webhook = Webhook::first();
+		// foreach ($this->getLeadsWithProblems() as $lead) {
+		// 	$content = @$lead->webhook_request->content;
+
+		// 	if ($content) {
+		// 		$controller->handler($webhook->token, new Request((array) ["lead_api" => $content]), $lead->id);
+		// 	}
+		// }
+		// DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+		// DB::statement('SET AUTOCOMMIT=1;');
+		// DB::statement('COMMIT;');
 	}
 
 	private function getLeadsWithProblems()
