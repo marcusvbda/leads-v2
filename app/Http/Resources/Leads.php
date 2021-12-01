9a617ba9f169ae7ca4b213d175ba25bf1961d163
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2021-12-01 14:55:20 -0300

    menos por pagina

--- app/Http/Resources/Leads.php
+++ app/Http/Resources/Leads.php
@@ -35,7 +35,7 @@ class Leads extends Resource
 
 	public function resultsPerPage()
 	{
-		return [20, 50, 100, 200, 500];
+		return [10, 20, 50, 100, 200, 500];
 	}
 
 	public function maxRowsExportSync()
