65a86af2b35279ab0b8de71e2d98bb1da293455c
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2021-12-03 13:24:14 -0300

    all in one

--- app/Http/Resources/Leads.php
+++ app/Http/Resources/Leads.php
@@ -37,7 +37,7 @@ class Leads extends Resource
 
 	public function resultsPerPage()
 	{
-		return [10, 20, 50, 100, 200, 500];
+		return [20, 50, 100, 200, 500];
 	}
 
 	public function maxRowsExportSync()
