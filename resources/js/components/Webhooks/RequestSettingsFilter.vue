83563c22b5b299bce3c738a74bb65327b2e73d26
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2021-12-09 10:03:03 -0300

    filter requests

---
+++ resources/js/components/Webhooks/RequestSettingsFilter.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="row mb-2">
+        <div class="col-12 my-3 d-flex">
+            <el-select
+                v-model="polo_id"
+                filterable
+                reserve-keyword
+                placeholder="Selecione um polo"
+                class="w-50 ml-auto"
+                clearable
+                @clear="makeFilter"
+            >
+                <el-option v-for="polo in polos" :key="polo.id" :label="polo.name" :value="polo.id" />
+            </el-select>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: ["polos"],
+    data() {
+        return {
+            polo_id: Number(this.$getUrlParams().polo_id) || ""
+        };
+    },
+    watch: {
+        filter: {
+            deep: true,
+            handler() {
+                this.makeFilter();
+            }
+        }
+    },
+    computed: {
+        route() {
+            if (!this.polo_id) {
+                return `${window.location.pathname}`;
+            }
+            return `${window.location.pathname}?polo_id=${this.polo_id}`;
+        }
+    },
+    methods: {
+        makeFilter() {
+            this.$loading({ text: "Aguarde ..." });
+            window.location.href = this.route;
+        }
+    }
+};
+</script>
