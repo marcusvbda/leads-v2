50c24eaee36409945f1a606fdb1d7146860688c0
mvbassalobre <bassalobre.vinicius@gmail.com>
2022-08-25 16:59:50 -0300

    corrige problema compilação mix

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,25 +14,25 @@ class DatabaseSeeder extends Seeder
 	 */
 	public function run()
 	{
-		Auth::logout();
-		DB::statement('SET AUTOCOMMIT=0;');
-		DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-		$this->call($this->getAllSeeder());
-		DB::statement('COMMIT;');
-		DB::statement('SET AUTOCOMMIT=1;');
-		DB::statement('SET FOREIGN_KEY_CHECKS=1;');
-		DB::statement('COMMIT;');
+		// Auth::logout();
+		// DB::statement('SET AUTOCOMMIT=0;');
+		// DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+		// $this->call($this->getAllSeeder());
+		// DB::statement('COMMIT;');
+		// DB::statement('SET AUTOCOMMIT=1;');
+		// DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+		// DB::statement('COMMIT;');
 	}
 
-	private function getAllSeeder()
-	{
-		$seeders = glob(database_path('/seeds/*.php'));
-		$_seeders = [];
-		foreach ($seeders as $file) {
-			require_once $file;
-			$classString = basename($file, '.php');
-			if ($classString != "DatabaseSeeder") $_seeders[] = get_class(new $classString);
-		}
-		return $_seeders;
-	}
+	// private function getAllSeeder()
+	// {
+	// 	$seeders = glob(database_path('/seeds/*.php'));
+	// 	$_seeders = [];
+	// 	foreach ($seeders as $file) {
+	// 		require_once $file;
+	// 		$classString = basename($file, '.php');
+	// 		if ($classString != "DatabaseSeeder") $_seeders[] = get_class(new $classString);
+	// 	}
+	// 	return $_seeders;
+	// }
 }
