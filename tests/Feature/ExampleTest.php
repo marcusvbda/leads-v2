dd21da233fab9d9d3493341f94346da5d5c660c6
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2023-06-01 19:51:22 -0300

    prepara test unitario

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
