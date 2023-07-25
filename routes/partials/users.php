b6f9cfbfcdb0c23c91ca759e1776264136fb288f
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2023-07-25 19:05:51 -0300

    limpa

--- routes/partials/users.php
+++ routes/partials/users.php
@@ -5,8 +5,3 @@ use App\Http\Controllers\Auth\UsersController;
 Route::group(["prefix" => "usuarios"], function () {
 	Route::get('cancel_invite/{code}', [UsersController::class, "cancelInvite"])->middleware(['hashids:code']);
 });
-
-Route::group(["prefix" => "profile"], function () {
-	Route::get('', [UsersController::class, "profile"]);
-	Route::post('', [UsersController::class, "profileEdit"]);
-});
